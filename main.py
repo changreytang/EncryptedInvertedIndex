f081be551e86a10479613bc86ac17a41518b172b
Rey Tang <tang.changrey@gmail.com>
2018-03-01 13:54:49 -0800

    allowed saving of index

--- main.py
+++ main.py
@@ -1,23 +1,28 @@
 #!/usr/bin/env python3
 
-import sys
-from index.inverted_index import InvertedIndex
+import sys, os
+from dotenv import find_dotenv, load_dotenv
+from lib.inverted_index import InvertedIndex
+
+load_dotenv(find_dotenv())
 
 def main(index):
     print("Query: ", end="", flush=True)
     for line in sys.stdin:
-        for doc_id in index.query(line[:-1]):
-            print(doc_id)
+        for document in index.query(line[:-1]).most_common(10):
+            print(document)
         print("Query: ", end="", flush=True)
 
-if len(sys.argv) != 2:
-    print("USAGE: ./main.py path_to_trec_dataset")
-    exit(0)
+# if len(sys.argv) != 2:
+#     print("USAGE: ./main.py path_to_trec_dataset")
+#     exit(0)
 
-trec_file_path = sys.argv[1]
+#trec_file_path = sys.argv[1]
+secret_key = os.environ.get("SECRET_KEY")
 
 if __name__ == "__main__":
-    index = InvertedIndex()
-    index.indexTREC(trec_file_path)
+    index = InvertedIndex('inverted_index')
+    #index.index_TREC(trec_file_path)
+    index.save_index()
     main(index)
 
